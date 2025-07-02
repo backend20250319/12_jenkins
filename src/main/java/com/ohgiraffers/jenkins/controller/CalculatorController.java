ba43d3b269e9c55a97c099e510f450dd5aae782d
macacolabs <monkey.ohgiraffers@gmail.com>
2025-07-02 21:59:53 +0900

    modify

--- src/main/java/com/ohgiraffers/jenkins/controller/CalculatorController.java
+++ src/main/java/com/ohgiraffers/jenkins/controller/CalculatorController.java
@@ -17,7 +17,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "I'm alive!😁🐋🙈✅";
+        return "I'm alive!😁🐋🙈✅🙈";
     }
 
     @GetMapping("/plus")
