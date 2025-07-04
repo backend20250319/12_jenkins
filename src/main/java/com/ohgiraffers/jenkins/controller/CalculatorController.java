be2b20d11f7f12bfa1ebd3f97249098eb8b515a9
macacolabs <monkey.ohgiraffers@gmail.com>
2025-07-04 10:12:33 +0900

    fix: controller source

--- src/main/java/com/ohgiraffers/jenkins/controller/CalculatorController.java
+++ src/main/java/com/ohgiraffers/jenkins/controller/CalculatorController.java
@@ -17,7 +17,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "ArgoCD Settings >.< 🙈😁🐋👌";
+        return "ArgoCD Change!! >.< 🙈😁🐋👌";
     }
 
     @GetMapping("/plus")
